52f7dd3baf941d1b41cb2ddc1f658adfde7de8be
goraan03 <gorangrcic72@gmail.com>
2025-07-23 00:28:55 +0200

    social media update

--- src/components/ContactSecction.jsx
+++ src/components/ContactSecction.jsx
@@ -72,13 +72,13 @@ export const ContactSection = () => {
                     <div className="pt-8">
                         <h4 className="font-medium mb-4"> Connect With Me </h4>
                         <div className="flex space-x-4 justify-center">
-                            <a href="#" target="_blank">
+                            <a href="https://www.linkedin.com/in/goran-grčić-7480862a6" target="_blank">
                                 <Linkedin />
                             </a>
-                            <a href="#" target="_blank">
+                            <a href="https://www.instagram.com/goraan03" target="_blank">
                                 <Instagram />
                             </a>
-                            <a href="#" target="_blank">
+                            <a href="https://facebook.com/goran.003" target="_blank">
                                 <Facebook />
                             </a>
                         </div>
